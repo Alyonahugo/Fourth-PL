582ff3d1e797fb6446f1b5ff3edd9970577330af
roman.PRV <roman.prv1@gmail.com>
2015-04-03 01:01:54 +0300

    ..

--- web/resources/js/aboutA.js
+++ web/resources/js/aboutA.js
@@ -1,5 +1,6 @@
-if (window.location.pathname == '/Fourth-PL/faces/templateFirst.xhtml')
-    $( document ).ready( function() {
-        $('#content').style('text-align', 'center');
-        $('table').style('margin-left', '40%');
-    } );
+// if (window.location.pathname == '/Fourth-PL/faces/templateFirst.xhtml')
+//     $( document ).ready( function() {
+//         $('#content').style('text-align', 'center');
+//         $('table').style('margin-left', '40%');
+//     } );
+console.log('checking');
