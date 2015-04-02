7a1af13a00e280af3da2b8a90a8ba02e154c1cf0
roman.PRV <roman.prv1@gmail.com>
2015-04-03 01:12:28 +0300

    cc

--- web/resources/js/aboutA.js
+++ web/resources/js/aboutA.js
@@ -6,11 +6,11 @@
 console.log('checking1');
 if (window.location.pathname == '/Fourth-PL/faces/templateFirst.xhtml') {
 	console.log('checking2');
-	$( document ).ready( function() {
+	
 		console.log('checking4');
         $('#content').style('text-align', 'center');
         $('table').style('margin-left', '40%');
-    } );
+    
 } else {
 	console.log('checking3');
 }
