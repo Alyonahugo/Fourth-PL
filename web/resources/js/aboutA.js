80888b8f9fd3b503bf6b07b5d56878814964dcd0
roman.PRV <roman.prv1@gmail.com>
2015-04-03 00:57:26 +0300

    added js file

---
+++ web/resources/js/aboutA.js
@@ -0,0 +1,5 @@
+if (window.location.pathname == '/Fourth-PL/faces/templateFirst.xhtml')
+    $( document ).ready( function() {
+        $('#content').style('text-align', 'center');
+        $('table').style('margin-left', '40%');
+    } );
