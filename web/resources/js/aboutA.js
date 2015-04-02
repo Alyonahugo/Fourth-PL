6d9f730362a7718f7228c491b8d8bf8fca5e4a9f
roman.PRV <roman.prv1@gmail.com>
2015-04-03 01:03:34 +0300

    qq

--- web/resources/js/aboutA.js
+++ web/resources/js/aboutA.js
@@ -3,4 +3,5 @@
 //         $('#content').style('text-align', 'center');
 //         $('table').style('margin-left', '40%');
 //     } );
+if (window.location.pathname == '/Fourth-PL/faces/templateFirst.xhtml')
 console.log('checking');
